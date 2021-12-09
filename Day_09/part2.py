e8b2aaec154a1501f5d9678eb2fbbe9262f733ba
Uklusi <aurelio.polinto@gmail.com>
2021-12-09 12:20:15 +0100

    Day 9
    The assumptions in this problem are nice

---
+++ Day_09/part2.py
@@ -0,0 +1,59 @@
+from AoCUtils import *
+
+
+result = 0
+partNumber = "2"
+
+writeToLog = False
+if writeToLog:
+    logFile = open("log" + partNumber + ".txt", "w")
+else:
+    logFile = "stdout"
+printLog = printLogFactory(logFile)
+
+heights = {}
+
+with open("input.txt", "r") as inputFile:
+    lines = inputFile.read().strip().split("\n")
+    for (y, line) in enumerate(lines):
+        line = line.strip()
+        for (x, char) in enumerate(line):
+            heights[Position(x, y)] = int(char)
+
+lowPoints = []
+
+for (x, y) in product(range(len(lines[0])), range(len(lines))):
+    p = MapPosition(x, y, frame=lines)
+    m = min([heights[q] for q in p.adjacent()])
+    if heights[p] < m:
+        lowPoints.append(p)
+
+basin = defaultdict(lambda: None)
+
+for (i, p) in enumerate(lowPoints):
+    queue = [p]
+    while queue:
+        q = queue.pop()
+        if basin[q] is None:
+            if heights[q] == 9:
+                basin[q] = -1
+            else:
+                basin[q] = i
+                queue.extend(q.adjacent())
+
+s = [sum([1 for v in basin.values() if i == v]) for i in set(basin.values()) if i != -1]
+
+s.sort(reverse=True)
+
+result = s[0] * s[1] * s[2]
+
+
+
+
+
+with open("output" + partNumber + ".txt", "w") as outputFile:
+    outputFile.write(str(result))
+    print(str(result))
+
+if writeToLog:
+    cast(TextIOWrapper, logFile).close()
