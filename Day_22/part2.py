210e665284647727d1ed8f353e70d6e64672d18d
Uklusi <aurelio.polinto@gmail.com>
2021-12-22 15:27:21 +0100

    Day 22 comments and type hints

--- Day_22/part2.py
+++ Day_22/part2.py
@@ -15,61 +15,65 @@ else:
     logFile = "stdout"
 printLog = printLogFactory(logFile)
 
-steps = []
+Coord = int
+Limit = tuple[Coord, Coord]
+Region = tuple[Limit, Limit, Limit]
+StepIndex = int
 
-class Lim(NamedTuple):
-    min: int
-    max: int
 
-class Range(NamedTuple):
-    x: Lim
-    y: Lim
-    z: Lim
+steps: list[tuple[Region, bool]] = []
 
 inputFileName = ("example.txt" if useExample else "input.txt")
 with open(inputFileName, "r") as inputFile:
     lines = inputFile.read().strip().split("\n")
     for line in lines:
-        line = line.strip().split()
-        turn = line[0]
-        line = line[1].split(",")
-        turn = (True if turn == "on" else False)
-        x = Lim(*map(int, line[0][2:].split("..")))
-        x = Lim(x.min, x.max + 1)
-        y = Lim(*map(int, line[1][2:].split("..")))
-        y = Lim(y.min, y.max + 1)
-        z = Lim(*map(int, line[2][2:].split("..")))
-        z = Lim(z.min, z.max + 1)
-        steps.append(((x, y, z), turn))
+        (newState, limitsString) = line.strip().split()
+        limits = limitsString.split(",")
+        turn = (True if newState == "on" else False)
 
-steps2 = [(step[0], i) for (i, step) in enumerate(steps)]
+        c: list[Limit] = []
+        for i in range(3):
+            (a, b) = limits[i][2:].split("..")
+            c.append((int(a), int(b) + 1))
+        steps.append((tuple(c), turn))
 
+steps2: list[tuple[Region, StepIndex]] = [(step[0], i) for (i, step) in enumerate(steps)]
 
-listMins = [sorted([(step[0][i].min, step[1]) for step in steps2]) for i in range(3)]
-listMaxs = [sorted([(step[0][i].max, step[1]) for step in steps2]) for i in range(3)]
+# listMins contains, for each dimension, the ordered list of lower bounds for each step,
+# and the step index that lower bound comes from
+# Same for listMaxs
+# Type signature is list[list[tuple[Coord, StepIndex]]]
+listMins = [sorted([(step[0][i][0], step[1]) for step in steps2], reverse=True) for i in range(3)]
+listMaxs = [sorted([(step[0][i][1], step[1]) for step in steps2], reverse=True) for i in range(3)]
 
-regionsep = [[], [], []]
+regionsep: list[list[tuple[Coord, set[StepIndex]]]] = [[], [], []]
+
 for i in range(3):
     curr = None
-    currsteps = set()
+    # currsteps is the set of steps s.t. their region intersects the strip
+    # curr × (-inf, inf) × (-inf, inf) (or with y or z depending on i)
+    currsteps: set[StepIndex] = set()
     while len(listMaxs[i]) > 0:
         if len(listMins[i]) == 0:
-            new = listMaxs[i].pop(0)
+            newTransition = listMaxs[i].pop()
             insert = False
         else:
-            new = listMins[i][0]
-            if new[0] > listMaxs[i][0][0]:
-                new = listMaxs[i].pop(0)
+            newTransition = listMins[i][-1]
+            if newTransition[0] > listMaxs[i][-1][0]:
+                newTransition = listMaxs[i].pop()
                 insert = False
             else:
-                new = listMins[i].pop(0)
+                newTransition = listMins[i].pop()
                 insert = True
+        # if newTransition comes from Mins, then we need to add the step to the set
+        # otherwise we need to remove it
 
         if insert:
-            currsteps.add(new[1])
+            currsteps.add(newTransition[1])
         else:
-            currsteps.remove(new[1])
-        new = new[0]
+            currsteps.remove(newTransition[1])
+
+        new = newTransition[0]
         if curr is None or curr < new:
             regionsep[i].append((new, currsteps.copy()))
             curr = new
@@ -77,24 +81,39 @@ for i in range(3):
             regionsep[i][-1] = (new, currsteps.copy())
         else:
             raise Exception("Value Error")
+# At the end, for each axis we have a list of (coord, s) s.t.
+# the set of steps intersecting with the strip is exactly s, and that is true
+# for all coords up to the next one in the list
 
-# print([len(regionsep[i]) for i in range(3)])
-# Generator to go around the memory issue
-regions = (
-    (
-        (x[1][0] - x[0][0]) * (y[1][0] - y[0][0]) * (z[1][0] - z[0][0]),
-        max(x[0][1] & y[0][1] & z[0][1])
-    )
-    for x in zip(regionsep[0], regionsep[0][1:])
-    for y in zip(regionsep[1], regionsep[1][1:])
-    for z in zip(regionsep[2], regionsep[2][1:])
-    if len(x[0][1] & y[0][1] & z[0][1]) > 0
-)
+# regions is the complete list of regions to be considered. Each region is non-overlapping,
+# contained is exactly one of the inctersections of all regions coming from the original steps,
+# and it is saved as its volume and the last step acting on the region (which is the only one that matters)
+# Actually it's a generator, because there are 198_360_483 regions and storing them in memory it's not worth
 
-for region in regions:
-    stepIndex = region[1]
-    if steps[stepIndex][1]:
-        result += region[0]
+# Hold on a sec, I wanna try using a for loop
+# This is quicker, but it's functionally equivalent to the code below
+for x in zip(regionsep[0], regionsep[0][1:]):
+    for y in zip(regionsep[1], regionsep[1][1:]):
+        for z in zip(regionsep[2], regionsep[2][1:]):
+            s = x[0][1] & y[0][1] & z[0][1]
+            if len(s) > 0 and steps[max(s)][1]:
+                result += (x[1][0] - x[0][0]) * (y[1][0] - y[0][0]) * (z[1][0] - z[0][0])
+
+# regions = (
+#     (
+#         (x[1][0] - x[0][0]) * (y[1][0] - y[0][0]) * (z[1][0] - z[0][0]),
+#         max(x[0][1] & y[0][1] & z[0][1])
+#     )
+#     for x in zip(regionsep[0], regionsep[0][1:])
+#     for y in zip(regionsep[1], regionsep[1][1:])
+#     for z in zip(regionsep[2], regionsep[2][1:])
+#     if len(x[0][1] & y[0][1] & z[0][1]) > 0
+# )
+
+# for region in regions:
+#     stepIndex = region[1]
+#     if steps[stepIndex][1]:
+#         result += region[0]
 
 with open("output" + partNumber + ".txt", "w") as outputFile:
     outputFile.write(str(result))
